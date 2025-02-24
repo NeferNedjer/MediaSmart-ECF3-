96d20caa5d1ad0a132549220fb03d2f69b7ac8ca
NeferNedjer <vincejurien@gmail.com>
2025-02-24 12:19:31 +0100

    add some comments

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
 
 
-//debut du code
+//AUTOLOAD DEPENDENCIES
+require_once './vendor/autoload.php';
+require_once './vendor/altorouter/altorouter/AltoRouter.php';
+
+//INITIALISATION OF ALTOROUTER
+$router = new AltoRouter();
+$router->setBasePath('/mediasmart');
+
