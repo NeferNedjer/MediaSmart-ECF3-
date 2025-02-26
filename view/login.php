225e07bf28a4528e9a5d6f73039eb5d3452ae3de
serge <joe.r.suliman@gmail.com>
2025-02-26 14:41:59 +0100

    4

--- view/login.php
+++ view/login.php
@@ -7,15 +7,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./assets/scss/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Login</h2>
+<!--     
     <form action="/mediasmart/login" method="POST">
+
+
         <label for="email">Email</label>
         <input type="text" name="email" id="email" required><br><br>
         <label for="password">Password</label>
         <input type="password" name="password" id="password" required><br><br>
         <input type="submit" name="submit" value="connection">
-    </form>
+
+    </form> -->
+
+    <div id="background" class='flex-container'>
+        <div class="login-container">
+            <h2 class="text-center custom-color">Connexion</h2>
+
+            <?php if (!empty($message)): ?>
+                <p style="color:red"><?php echo htmlspecialchars($message); ?></p>
+            <?php endif; ?>
+
+            <form action="/mediasmart/login" method="POST">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Adresse e-mail</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">Mot de passe</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe" required>
+                </div>
+
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="remember" name="remember">
+                    <label class="form-check-label" for="remember">Se souvenir de moi</label>
+                </div>
+
+               <a href="homepage"> <button type="submit" class="btn-login ">Connexion</button></a>
+            </form>
+
+            <div class="text-center mt-3">
+                <a id="register" href="register" class="text-decoration-none text-center">Créer un compte</a>
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
