12de514120883f0ef127c318ed6133aa37037aef
serge <joe.r.suliman@gmail.com>
2025-02-28 09:21:16 +0100

    problm resolu

--- view/login.php
+++ view/login.php
@@ -11,17 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<!--     
-    <form action="/mediasmart/login" method="POST">
 
-
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email" required><br><br>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required><br><br>
-        <input type="submit" name="submit" value="connection">
-
-    </form> -->
 
     <div id="background" class='flex-container'>
         <div class="login-container">
