5389fe66b421e7a3f1dd635d5dffe795a130b935
unknown <ahlem.boutayeb@outalook.fr>
2025-03-03 13:38:50 +0100

    formulaire employé html et css

--- view/createEmployee.php
+++ view/createEmployee.php
@@ -4,23 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Creation d'employee</title>
+    <link rel="stylesheet" href="../assets/scss/style.css">
 </head>
 <body>
-    <form action="/mediasmart/createEmployee" method="POST">
-
-    <label for="name">Nom :</label>
-    <input type="text" name="name"><br><br>
-
-    <label for="first_name">Prénom :</label>
-    <input type="text" name="first_name"><br><br>
-
-    <label for="password">Password :</label>
-    <input type="password" name="password"><br><br>
-
-    <label for="confpassword">Confirmez le password :</label>
-    <input type="password" name="confpassword" id="confpassword"><br><br>
-
-    <input type="submit" value="Valider">
-    </form>
+<div id=background>
+    <div id="createEmployee" class='flex-container'>
+        <h2>Création d'un employé</h2> <br><br>
+        <form action="/mediasmart/createEmployee" method="POST">
+            <div class="mb-3">
+            <label for="name">Nom :</label>
+            <input type="text" name="name"><br><br>
+            </div>
+            <div class="mb-3">       
+            <label for="first_name">Prénom :</label>
+            <input type="text" name="first_name"><br><br>
+            </div>
+            <div class="mb-3">
+            <label for="password">Password :</label>
+            <input type="password" name="password"><br><br>
+            </div>
+            <div class="mb-3">
+            <label for="confpassword">Confirmez le password :</label>
+            <input type="password" name="confpassword" id="confpassword"><br><br>
+            </div>
+            <div class="mb-3">
+            <input type="submit" value="Valider">
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
