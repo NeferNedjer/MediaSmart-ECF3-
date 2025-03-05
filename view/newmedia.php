ef0edfbb56b1393291d901521d37ac0582c36397
serge <joe.r.suliman@gmail.com>
2025-03-05 16:24:49 +0100

    dash

--- view/newmedia.php
+++ view/newmedia.php
@@ -4,16 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gestion media</title>
     <link rel="stylesheet" href="../assets/scss/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
 <body>
 
-
-    <?php if (isset($error)): ?>
-        <p><?php echo $error; ?></p>
-    <?php endif ?>
     <section id="side-bar-dash">
         <section id="navbar-left">
             <ul class="nav-menu">
@@ -42,84 +39,114 @@
                 </li>
             </ul>
         </section>
-        <section id="dashboard" style="height: 100%; width:100%;  border-top-left-radius: 28px;
-  border-bottom-left-radius: 8px;">
-            <div>
-                <input type="text" name="search-product" id="search-product">
-            </div>
-            <ul class="list-keyword-dashboard">
-                <li>Livre</li>
-                <li>DVD</li>
-                <li>Film</li>
-                <li> <button class="btn-add-dashboard">
+        <section id="dashboard">
+            <section id="grid-user-gestion">
+                <section id="left-grid">
+                    <h1>Gestion des Media</h1>
+                    <button class="btn-add-dashboard">
                         <span>+</span>
-                    </button></li>
-            </ul>
+                    </button>
+
+                    <div class="user-container">
+                        <ul class="user-gestion-list">
+                            <li>id</li>
+                            <li>Titre</li>
+                            <li>Auteur</li>
+                            <li>Catégorie</li>
+
+                        </ul>
+                    </div>
 
 
+                    <div class="user-row">
+                        <div class="user-dashboard">
+                            <p class="id-user-dashboard">#14567</p>
+                            <p class="name-dashboard"></p>
+                            <p class="date-dashboard">Serge</p>
+                            <p class="livre-non">Roman</p>
+                            <button type="submit" id="more-dashboard">More</button>
+                        </div>
+                    </div>
+                </section>
+                <section id="right-grid">
+                    <h2>Detail produits</h2>
+                    <div class="detail-product">
+                        <figure>
+                            <img src="../assets/img/livre-cuisine-tout-en-pot-jean-pierre-dezavelle.webp" alt="">
+                        </figure>
+                        <div class="contain-detail-product">
+                            <p class="title-detail-product">Livre de cuise</p>
+                            <p class="auteur-detail-product">Lorette</p>
+                            <p class="description-auteur-product">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae eius eos facilis. Laboriosam quidem accusamus sunt ullam ipsam tenetur unde error quo deserunt et.</p>
+                        </div>
+                    </div>
+
+
+
+
+
+
+                </section>
+            </section>
+            <form action="mediasmart/media/create" method="POST" enctype="multipart/form-data" id="form-create-media" class="media-form" style="display: none;">
+                <h2 class="form-title">Ajouter un nouveau média</h2>
+                <div class="form-group">
+                    <label for="title">Titre :</label>
+                    <input type="text" name="title" id="title" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="id_author">Auteur :</label>
+                    <input type="text" name="id_author" id="id_author" class="form-select">
+
+                </div>
+
+                <div class="form-group category-group">
+                    <label class="group-label">Catégorie :</label>
+                    <div class="radio-container">
+                        <?php foreach ($categories as $category): ?>
+                            <div class="radio-item">
+                                <input type="radio" name="id_category" id="id_category_<?php echo $category->getId_category(); ?>" value="<?php echo $category->getId_category(); ?>" class="radio-input">
+                                <label for="id_category_<?php echo $category->getId_category(); ?>" class="radio-label"><?php echo $category->getName(); ?></label>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="id_subcategory">Genre :</label>
+                    <select name="id_subcategory" id="id_subcategory" class="form-select">
+                        <option value="">Sélectionner un genre</option>
+                        <?php foreach ($subcategories as $subcategory): ?>
+                            <option value="<?php echo $subcategory->getId_subcategory(); ?>" data-category-id="<?php echo $subcategory->getId_category(); ?>"><?php echo $subcategory->getTheme(); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description :</label>
+                    <textarea name="description" id="description" class="form-textarea" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="image" class="file-label">Image :</label>
+                    <div class="file-upload">
+                        <input type="file" name="image" id="image" class="file-input" required>
+                        <span class="file-custom">Choisir un fichier</span>
+                    </div>
+                </div>
+
+                <div class="form-actions">
+                    <button type="submit" class="btn-submit">Créer</button>
+                </div>
+            </form>
 
         </section>
     </section>
 
-    <form action="mediasmart/media/create" method="POST" enctype="multipart/form-data" id="form-create-media" class="media-form" style="display: none;">
-        <h2 class="form-title">Ajouter un nouveau média</h2>
-        <div class="form-group">
-            <label for="title">Titre :</label>
-            <input type="text" name="title" id="title" class="form-control" required>
-        </div>
+    <script src="../assets/js/dashboard.js"></script>
+    <script src="./assets/js/newmedia.js"></script>
 
-        <div class="form-group">
-            <label for="id_author">Auteur :</label>
-            <select name="id_author" id="id_author" class="form-select">
-                <option value="">Sélectionner un auteur</option>
-                <?php foreach ($authors as $author): ?>
-                    <option value="<?php echo $author->getId_author(); ?>"><?php echo $author->getName(); ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-
-        <div class="form-group category-group">
-            <label class="group-label">Catégorie :</label>
-            <div class="radio-container">
-                <?php foreach ($categories as $category): ?>
-                    <div class="radio-item">
-                        <input type="radio" name="id_category" id="id_category_<?php echo $category->getId_category(); ?>" value="<?php echo $category->getId_category(); ?>" class="radio-input">
-                        <label for="id_category_<?php echo $category->getId_category(); ?>" class="radio-label"><?php echo $category->getName(); ?></label>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="id_subcategory">Genre :</label>
-            <select name="id_subcategory" id="id_subcategory" class="form-select">
-                <option value="">Sélectionner un genre</option>
-                <?php foreach ($subcategories as $subcategory): ?>
-                    <option value="<?php echo $subcategory->getId_subcategory(); ?>" data-category-id="<?php echo $subcategory->getId_category(); ?>"><?php echo $subcategory->getTheme(); ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="description">Description :</label>
-            <textarea name="description" id="description" class="form-textarea" required></textarea>
-        </div>
-
-        <div class="form-group">
-            <label for="image" class="file-label">Image :</label>
-            <div class="file-upload">
-                <input type="file" name="image" id="image" class="file-input" required>
-                <span class="file-custom">Choisir un fichier</span>
-            </div>
-        </div>
-
-        <div class="form-actions">
-            <button type="submit" class="btn-submit">Créer</button>
-        </div>
-    </form>
-    <!-- 
-    <a href="<?php echo $router->generate('dashboard-employee') ?>">retour</a> -->
-    <script src="../assets/js/newmedia.js"></script>
 
 </body>
 
